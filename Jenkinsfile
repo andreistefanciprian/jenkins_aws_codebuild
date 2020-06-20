c56919223cb61e04b07c5c9322bbe6ec45e04d95
Stefan Andrei <andreistefanciprian@gmail.com>
2020-06-20 12:12:04 +0100

    update function

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,26 +1,33 @@
 pipeline {
+
    agent any
-    environment {
-        AWS_ACCESS_KEY_ID = credentials('aws_access_key')
-        AWS_REGION = credentials('aws_region')
-        AWS_DEFAULT_REGION = credentials('aws_region')
-        AWS_SECRET_ACCESS_KEY = credentials('aws_secret_key')
-    }
+
+   environment {
+      AWS_ACCESS_KEY_ID = credentials('aws_access_key')
+      AWS_REGION = credentials('aws_region')
+      AWS_DEFAULT_REGION = credentials('aws_region')
+      AWS_SECRET_ACCESS_KEY = credentials('aws_secret_key')
+   }
+
    stages {
-      stage('Hello') {
+
+      stage('CLEAN WORKING DIR & GIT CHECKOUT') {
          steps {
-            echo 'issue AWS commands'
-            sh 'aws codebuild list-projects'
-            // sh 'aws codebuild start-build --project-name codebuildtest-MessageUtil'
+               cleanWs()  
+               checkout scm
+         }
+      }
 
-            // generate txt file with codebuild projects
+      stage('Execute CodeBuild projects in AWS') {
+         steps {
+
+            echo 'Parse yaml file with python3 script and output codebuild projects in txt file...'
             sh 'python3 parse_yaml.py'
-            sh 'ls -larth'
-            sh 'cat codebuild_projects.txt'
 
-            // execute codebuild projects
+            echo 'Execute codebuild projects...'
             sh 'bash execute_codebuild_projects.sh'
          }
       }
+
    }
 }
--- parse_yaml.py
+++ parse_yaml.py
@@ -1,36 +1,43 @@
-
-
 import yaml
 import os
 
-
-def parse_yaml(file_path):
+def parse_yaml(yaml_file, txt_file):
     """
-    Parse yaml file and output codebuild steps in txt file.
+    Parse yaml file and output AWS codebuild steps in txt file.
     """
 
-    result = False
-
-    if os.path.exists(file_path):
-        with open(file_path, 'r') as file:
+    if os.path.exists(yaml_file):
+        with open(yaml_file, 'r') as file:
             try:
                 read_yaml = yaml.full_load(file)
             except Exception as e:
                 raise e
             else:
+                create_txt_file(txt_file)
                 for codebuild_project in read_yaml['codebuild_projects']:
-                    # write to file
-                    with open('codebuild_projects.txt', 'a+') as file:
+                    with open(txt_file, 'a+') as file:
                          file.write(codebuild_project + '\n')
-                        # print(codebuild_project)
-                result = True
             finally:
-                return result
+                with open(txt_file, 'r') as file:
+                    result = file.read()
+                    return result
     else:
-        result = f"File/Directory {file_path} does not exist!"
+        result = f"{yaml_file} file does not exist!"
         return result
+
+def create_txt_file(txt_file):
+    """
+    Creates empty txt file.
+    """
+    with open(txt_file, 'w') as file:
+        pass
+
+if __name__ == "__main__":
     
-# tests
-file_path = 'file.yaml'
-parse = parse_yaml(file_path)
-print(parse)
+    # variables
+    txt_file = os.path.join(os.getcwd(), 'codebuild_projects.txt')
+    yaml_file = os.path.join(os.getcwd(), 'file.yaml')
+
+    #  execute function
+    parse = parse_yaml(yaml_file, txt_file)
+    print(parse)
