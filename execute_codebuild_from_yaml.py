cdec173150df170ece00fd7d62112ab959dd0b42
Stefan Andrei <andreistefanciprian@gmail.com>
2020-06-20 14:40:27 +0100

    refactor check build function

--- execute_codebuild_from_yaml.py
+++ execute_codebuild_from_yaml.py
@@ -8,47 +8,44 @@ def execute_codebuild_from_yaml(yaml_file):
     """
     Execute AWS codebuild projects provided in yaml file.
     """
+
     if os.path.exists(yaml_file):
         with open(yaml_file, 'r') as file:
             try:
                 read_yaml = yaml.full_load(file)
+                print(f"Reading yaml file at: {yaml_file}")
             except Exception as e:
+                print(str(e))
                 raise e
             else:
+                print("Connecting to AWS to execute the CodeBuild projects...")
                 client = boto3.client('codebuild')
                 codebuild_projects = client.list_projects()
                 codebuild_projects = codebuild_projects['projects']
 
                 for codebuild_project in read_yaml['codebuild_projects']:
                     if codebuild_project in codebuild_projects:
+                        print(f"CodeBuild Project {codebuild_project} is available in AWS CodeBuild Project list.")
+                        
+                        # start CodeBuild project build
                         client.start_build(projectName=codebuild_project)
-                        # print(f"Started {get_last_build_results(client, codebuild_project)}")
 
-                        # print results of last build
-                        # last_build_results = get_last_build_results(client, codebuild_project)
-                        # for k,v in last_build_results.items():
-                        #     print(f"{k}: {v}")
+                        last_build_results = get_last_build_results(client, codebuild_project)
+                        print(f"Starting AWS CodeBuild Project {codebuild_project} build {last_build_results['Build Number']} ...")
 
-                        # while last_build_results['Build Status'] == 'SUCCEEDED':
-                        #     print(f'Build {last_build_results['Build Number']} of {last_build_results['Build Project Name']} is a success!')
+                        # verify build status
+                        verify_build_status(client, codebuild_project)
+                        # while True:
+                        #     last_build_results = get_last_build_results(client, codebuild_project)
+                        #     status_msg = f"CodeBuild Project: {last_build_results['Build Project Name']} -- Build Number: {last_build_results['Build Number']} -- Status: {last_build_results['Build Status']}"
 
-                        # {'Build Number': 13, 'Build Start Time': datetime.datetime(2020, 6, 20, 14, 6, 46, 284000, tzinfo=tzlocal()), 'Build Status': 'IN_PROGRESS', 'Build Project Name': 'newproj-test'}
-                        # status_msg = f"CodeBuild Project: {last_build_results['Build Project Name']} -- Build Number: {last_build_results['Build Number']} -- Status: {last_build_results['Build Status']}"
-                        # print(status_msg)
-
-                        while True:
-                            last_build_results = get_last_build_results(client, codebuild_project)
-                            status_msg = f"CodeBuild Project: {last_build_results['Build Project Name']} -- Build Number: {last_build_results['Build Number']} -- Status: {last_build_results['Build Status']}"
-
-                            if last_build_results['Build Status'] == 'SUCCEEDED':
-                                # print(f"Build {last_build_results['Build Number']} {last_build_results['Build Status']}")
-                                print(status_msg)
-                                break
-                            else:
-                                # print(f"Build {last_build_results['Build Number']} {last_build_results['Build Status']}")
-                                print(status_msg)
-                                time.sleep(10)
-                                continue
+                        #     if last_build_results['Build Status'] == 'SUCCEEDED':
+                        #         print(status_msg)
+                        #         break
+                        #     else:
+                        #         print(status_msg)
+                        #         time.sleep(10)
+                        #         continue
                         
                         # display build run result in json format
                         # build_result = dict2json(build_data)
@@ -60,7 +57,28 @@ def execute_codebuild_from_yaml(yaml_file):
 
     else:
         result = f"{yaml_file} file does not exist!"
-        return result
+        print(result)
+
+def verify_build_status(client, codebuild_project):
+    """
+    This function verifies the status of the last CodeBuild build until it is successfull.
+    """
+
+    time_interval = 2
+    # max_time = to be implemented
+
+    while True:
+        last_build_results = get_last_build_results(client, codebuild_project)
+        status_msg = f"CodeBuild Project: {last_build_results['Build Project Name']} -- Build Number: {last_build_results['Build Number']} -- Status: {last_build_results['Build Status']}"
+
+        if last_build_results['Build Status'] == 'SUCCEEDED':
+            print(status_msg)
+            break
+        else:
+            print(status_msg)
+            time.sleep(time_interval)
+            continue
+
 
 def get_last_build_results(client, codebuild_project):
     """
