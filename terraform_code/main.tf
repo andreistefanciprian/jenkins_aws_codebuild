cecc6eba22a562293c9e7a9393fea6bb8fc78a0f
Stefan Andrei <andreistefanciprian@gmail.com>
2020-06-24 12:06:27 +0100

    update

--- terraform_code/main.tf
+++ terraform_code/main.tf
@@ -1,1 +1,40 @@
-s
+# terraform cloud provider
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.region
+}
+
+# terraform backend
+terraform {
+  backend "s3" {
+    bucket = "s3-tfstate-95653"
+    key    = "static/static.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "dynamodb-tfstate-lock-95653"
+  }
+}
+
+# aws vpc
+data "aws_availability_zones" "available" {}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = var.name
+  cidr = var.cidr_block
+  
+  azs = slice(data.aws_availability_zones.available.names,0,var.subnet_count)
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
+
---
+++ terraform_code/variables.tf
@@ -0,0 +1,30 @@
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "name" {
+  default = "lab2"
+}
+
+
+variable "subnet_count" {
+  default = 2
+}
+
+variable "cidr_block" {
+  default = "10.0.0.0/16"
+}
+
+variable "private_subnets" {
+  type = list
+  default = ["10.0.1.0/24", "10.0.3.0/24"]
+}
+
+variable "public_subnets" {
+  type = list
+  default = ["10.0.31.0/24", "10.0.33.0/24"]
+}
