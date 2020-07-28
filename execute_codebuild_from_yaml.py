11feb58fcd1b1153639fb83f862d6d1bed1c325a
Stefan Andrei <andreistefanciprian@gmail.com>
2020-07-28 22:57:34 +0100

    update

--- execute_codebuild_from_yaml.py
+++ execute_codebuild_from_yaml.py
@@ -29,7 +29,7 @@ def main(yaml_file, arn, session_name, aws_region, external_id, duration_seconds
                 # codebuild_projects = get_codebuild_projects_from_aws(client)
 
                 session = AwsSession(arn, session_name, aws_region, external_id, duration_seconds)
-                role = session.assume_role()
+                session.assume_role()
                 # print(role)
                 # client = role.client('codebuild')
                 codebuild_projects = session.get_codebuild_projects_from_aws()
@@ -50,18 +50,19 @@ def main(yaml_file, arn, session_name, aws_region, external_id, duration_seconds
                         # start_build(session_client, codebuild_project)
 
                         session = AwsSession(arn, session_name, aws_region, external_id, duration_seconds)
-                        role = session.assume_role()
+                        session.assume_role()
                         session.start_build(codebuild_project)
 
                         # # verify CodeBuild build status
                         # status = verify_build_status(session_client, codebuild_project)
-                        status = session.verify_build_status(codebuild_project)
-                        if status != "SUCCEEDED": 
-                            log(f"CodeBuild Project {codebuild_project} failed!")
-                            sys.exit(1)
-                        else:
-                            log(f"CodeBuild Project {codebuild_project}: {status}!")
-                            continue
+                        
+                        # status = session.verify_build_status(codebuild_project)
+                        # if status != "SUCCEEDED": 
+                        #     log(f"CodeBuild Project {codebuild_project} failed!")
+                        #     sys.exit(1)
+                        # else:
+                        #     log(f"CodeBuild Project {codebuild_project}: {status}!")
+                        #     continue
 
                     else:
                         log(f"\n{codebuild_project} is not available in AWS CodeBuild Project list.")
