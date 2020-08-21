1a72735c14bd86984fdecba68db3b8d64fd0579f
Stefan Andrei <andreistefanciprian@gmail.com>
2020-08-21 21:29:15 +0100

    update

--- terraform_code/codebuild/main.tf
+++ terraform_code/codebuild/main.tf
@@ -5,9 +5,9 @@ provider "aws" {
 
   assume_role {
     role_arn    = var.cloud_assume_role ? var.arn_role : null
-    external_id = var.extenal_id
-    session_name = var.session_name
-    duration_seconds = var.session_duration
+    external_id = var.cloud_assume_role ? var.extenal_id : null
+    session_name = var.cloud_assume_role ? var.session_name : null
+    duration_seconds = var.cloud_assume_role ? var.session_duration : null
   }
 
 }
@@ -18,7 +18,7 @@ terraform {
     bucket         = "s3-tfstate-83086"
     key            = "codebuild/codebuild.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "dynamodb-tfstate-lock-83086"
+    dynamodb_table = "dynamodb-tfstate-lock-83087"
   }
 }
 
