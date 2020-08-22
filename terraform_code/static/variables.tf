e1515737574661a84ebdd2abe1db6e09ace0648e
Stefan Andrei <andreistefanciprian@gmail.com>
2020-08-22 11:59:44 +0100

    update arn role var

--- terraform_code/static/variables.tf
+++ terraform_code/static/variables.tf
@@ -32,7 +32,7 @@ variable "cloud_assume_role" {
 
 variable "arn_role" {
   type = string
-  default = "arn:aws:iam::1234567890:role/test-role"
+  default = "arn:aws:iam::396667666940:role/test-role"
 }
 
 variable "extenal_id" {
