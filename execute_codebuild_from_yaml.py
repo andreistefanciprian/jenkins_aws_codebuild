f315cb5641e41a9cd10122bab12dafb21be4498d
Stefan Andrei <andreistefanciprian@gmail.com>
2020-11-08 16:53:21 +0000

    formatting

--- execute_codebuild_from_yaml.py
+++ execute_codebuild_from_yaml.py
@@ -85,16 +85,18 @@ class AwsSession:
     Sending commands to AWS.
     """
 
-    def __init__(self, aws_account, aws_iam_role, aws_region, duration_seconds=None, external_id=None, session_name=None, logs_group_name=None):
+    def __init__(self, aws_account, aws_iam_role, aws_region, sts_duration=None, sts_external_id=None, sts_session_name=None, logs_group_name=None):
 
-        # aws assume role instance vars
+        # aws instance vars
         self.aws_account = aws_account
         self.aws_iam_role = aws_iam_role
         self.arn = f'arn:aws:iam::{aws_account}:role/{aws_iam_role}'
-        self.session_name = 'jenkins_session' if session_name is None else session_name
+        self.sts_session_name = 'jenkins_session' if sts_session_name is None else sts_session_name
         self.aws_region = aws_region
-        self.external_id = 'jenkins_id' if external_id is None else external_id
-        self.duration_seconds = 3600 if duration_seconds is None else duration_seconds
+
+        # aws assume role instance vars
+        self.sts_external_id = 'jenkins_id' if sts_external_id is None else sts_external_id
+        self.sts_duration = 3600 if sts_duration is None else sts_duration
         self._sts_client = None
         self._sts_session = self._assume_role()
 
@@ -122,7 +124,7 @@ class AwsSession:
             log(str(e))
             raise
         else:
-            response = self._sts_client .assume_role(RoleArn=self.arn, RoleSessionName=self.session_name, DurationSeconds=self.duration_seconds, ExternalId=self.external_id)
+            response = self._sts_client .assume_role(RoleArn=self.arn, RoleSessionName=self.sts_session_name, DurationSeconds=self.sts_duration, ExternalId=self.sts_external_id)
 
             session = Session(aws_access_key_id=response['Credentials']['AccessKeyId'],
                         aws_secret_access_key=response['Credentials']['SecretAccessKey'],
@@ -322,18 +324,18 @@ class AwsSession:
 if __name__ == "__main__":
 
     # define vars
+    yaml_file = os.path.join(os.getcwd(), 'codebuild_projects.yaml')
     aws_account = sys.argv[1] if len(sys.argv) == 2 else sys.exit('AWS ARN Role has to be provided as positional parameter!')
     aws_iam_role = 'tf_role'
-    yaml_file = os.path.join(os.getcwd(), 'codebuild_projects.yaml')
-    session_name = 'funky_test'
     aws_region = 'us-east-1'
-    external_id = 'smth'
-    duration_seconds = 3600
+    sts_session_name = 'funky_test'
+    sts_external_id = 'smth'
+    sts_duration = 3600
     
     # parse yaml file
     codebuild_projects_from_yaml = parse_yaml(yaml_file, 'codebuild_projects')
 
     # execute codebuild projects
-    main(codebuild_projects_from_yaml, aws_account=aws_account, aws_iam_role=aws_iam_role, session_name=session_name, aws_region=aws_region, external_id=external_id, duration_seconds=duration_seconds)
-    # main(codebuild_projects_from_yaml, aws_account=aws_account, aws_iam_role=aws_iam_role, aws_region=aws_region, session_name=session_name)
+    main(codebuild_projects_from_yaml, aws_account=aws_account, aws_iam_role=aws_iam_role, sts_session_name=sts_session_name, aws_region=aws_region, sts_external_id=sts_external_id, sts_duration=sts_duration)
+    # main(codebuild_projects_from_yaml, aws_account=aws_account, aws_iam_role=aws_iam_role, aws_region=aws_region, sts_session_name=sts_session_name)
     
